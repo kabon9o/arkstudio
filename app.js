b99d49e1c6173af0fd9ff7954a9e0767ad24870b
Ryan <ryan.kabongo69@gmail.com>
2026-06-17 15:13:26 -0400

    ark moved, grew

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.use(express.static('client'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get("/aboutMe", (req, res) => {
+    res.sendFile( path.join(__dirname, "client", "AboutMe.html"));
+});
+
+
+//app.get ("/page" (req, res) =>{
+//  res.send("page") });
